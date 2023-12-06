3f314860e9a28691e9db2882ab96abd6f83fb768
JayvonK <jaykhuth@gmail.com>
2023-12-06 02:44:36 -0800

    iconDisplay

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,11 @@ import { TimeOnly } from "./timOnly.js";
 import { ChangeIcon } from "./changeIcon.js";
 
 let mainIcon = document.getElementById("mainIcon");
+let day1Icon = document.getElementById("day1Icon");
+let day2Icon = document.getElementById("day2Icon");
+let day3Icon = document.getElementById("day3Icon");
+let day4Icon = document.getElementById("day4Icon");
+let day5Icon = document.getElementById("day5Icon");
 let currTemp = document.getElementById("currTemp");
 let currCity = document.getElementById("currCity");
 let currWeather = document.getElementById("currWeather");
@@ -97,7 +102,19 @@ async function FiveDayApiCall(a, b, c) {
     //Displaying Max temps for 5 day forecast
     highTemp1.innerText = Math.floor(KelvinConvert(HighTemp(data.list[0].main.temp_max, data.list[1].main.temp_max, data.list[2].main.temp_max, data.list[3].main.temp_max, data.list[4].main.temp_max, data.list[5].main.temp_max, data.list[6].main.temp_max, data.list[7].main.temp_max))) + "°F";
 
+    let tempMax = Math.floor(KelvinConvert(HighTemp(data.list[0].main.temp_max, data.list[1].main.temp_max, data.list[2].main.temp_max, data.list[3].main.temp_max, data.list[4].main.temp_max, data.list[5].main.temp_max, data.list[6].main.temp_max, data.list[7].main.temp_max))) + "°F";
+
+
+
+
     highTemp2.innerText = Math.floor(KelvinConvert(HighTemp(data.list[8].main.temp_max, data.list[9].main.temp_max, data.list[10].main.temp_max, data.list[11].main.temp_max, data.list[12].main.temp_max, data.list[13].main.temp_max, data.list[14].main.temp_max, data.list[15].main.temp_max))) + "°F";
+
+    let tempMax2 = Math.floor(KelvinConvert(HighTemp(data.list[8].main.temp_max, data.list[9].main.temp_max, data.list[10].main.temp_max, data.list[11].main.temp_max, data.list[12].main.temp_max, data.list[13].main.temp_max, data.list[14].main.temp_max, data.list[15].main.temp_max)));
+
+
+
+
+
 
     highTemp3.innerText = Math.floor(KelvinConvert(HighTemp(data.list[16].main.temp_max, data.list[17].main.temp_max, data.list[18].main.temp_max, data.list[19].main.temp_max, data.list[20].main.temp_max, data.list[21].main.temp_max, data.list[22].main.temp_max, data.list[23].main.temp_max))) + "°F";
 
@@ -114,6 +131,19 @@ async function FiveDayApiCall(a, b, c) {
     lowTemp4.innerText = Math.floor(KelvinConvert(LowestTemp(data.list[24].main.temp_min, data.list[25].main.temp_min, data.list[26].main.temp_min, data.list[27].main.temp_min, data.list[28].main.temp_min, data.list[29].main.temp_min, data.list[30].main.temp_min, data.list[31].main.temp_min))) + "°F";
 
     lowTemp5.innerText = Math.floor(KelvinConvert(LowestTemp(data.list[32].main.temp_min, data.list[33].main.temp_min, data.list[34].main.temp_min, data.list[35].main.temp_min, data.list[36].main.temp_min, data.list[37].main.temp_min, data.list[38].main.temp_min, data.list[39].main.temp_min))) + "°F";
+
+
+    //Day 1 Icon and Temperature
+
+    for(let i = 0; i < 7; i++){
+        let maxTemp = Math.floor(KelvinConvert(data.list[i].main.temp_max));
+        console.log(maxTemp);
+        console.log(tempMax);
+        if(maxTemp === tempMax){
+            day1Icon.src = ChangeIcon(data.list[i].weather[0].icon);
+            console.log(ChangeIcon(data.list[i].weather[0].icon));
+        }
+    }
 
 }
 
