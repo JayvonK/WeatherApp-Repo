399682c99ae51b4670bf7e72f2f2c93b4acb215c
JayvonnK <jaykhuth@gmail.com>
2024-01-15 20:00:30 -0800

    fetch

--- scripts/addToDB.js
+++ scripts/addToDB.js
@@ -1,4 +1,11 @@
+
+
 async function AddToDB(cityName)
 {
-    const promise = await fetch('http://localhost:5056/')
+    const promise = await fetch(`http://localhost:5056/AddCity/${cityName}`);
+    const data = await promise.json();
+
 }
+
+
+Exp
