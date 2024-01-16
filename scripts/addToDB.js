ef6fd8f6fb13d09ee23b115b172b7c24bd3951f7
JayvonnK <jaykhuth@gmail.com>
2024-01-15 20:08:43 -0800

    export

--- scripts/addToDB.js
+++ scripts/addToDB.js
@@ -7,8 +7,8 @@ async function AddToDB(cityName)
 
 async function DeleteFav(cityName)
 {
-    const promise = await fetch(`http://localhost:5056/FavoriteCities/AddCity/${cityName}`);
+    const promise = await fetch(`http://localhost:5056/FavoriteCities/DeleteCity/${cityName}`);
     const data = await promise.json();
 }
 
-export { AddToDB }
+export { AddToDB, DeleteFav }
