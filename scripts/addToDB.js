68bd9b0e115a732cce39ff2bbc8652622de4dbeb
JayvonK <jaykhuth@gmail.com>
2024-01-20 20:52:34 -0800

    save

--- scripts/addToDB.js
+++ scripts/addToDB.js
@@ -1,15 +1,13 @@
 
 async function AddToDB(cityName)
 {
-    const promise = await fetch(`http://localhost:5056/FavoriteCities/AddCity/${cityName}`)
-    const data = await promise.json();
+    const promise = await fetch(`http://localhost:5056/FavoriteCities/AddCity/${cityName}`, {method: "POST"});
     console.log("I freaking work!");
 }
 
 async function DeleteFav(cityName)
 {
-    const promise = await fetch(`http://localhost:5056/FavoriteCities/DeleteCity/${cityName}`)
-    const data = await promise.json();
+    const promise = await fetch(`http://localhost:5056/FavoriteCities/DeleteCity/${cityName}`);
     console.log("I delted something");
 }
 
